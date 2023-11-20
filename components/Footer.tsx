4ddedfa17a28ff4854cc28008a864e334cadf215
cc lucky <2226114701@qq.com>
2023-11-20 20:45:28 +0800

    页脚修改成issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <div className="w-full h-32 bg-purple-300 flex items-center justify-center text-4xl font-medium">
-      我是腿
+    <div className="w-full h-32 bg-purple-300 flex items-center justify-center">
+      <Link
+        href="https://github.com/ccclucky/print-client/issues"
+        className="font-bold text-base"
+        target="_blank"
+      >
+        issues
+      </Link>
     </div>
   );
 }
